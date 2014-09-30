46274886b79f44e18f337464f0077d477c8cc711
Martina Radeva <martaradeva@yahoo.com>
2014-09-30 09:34:11 +0300

    user input check fixed

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
-require taylor
+require './taylor'
 
-until x.is_a? Fixnum do
+x=0
+# until x.is_a? Fixnum do
+until x!=0 do
   puts "Enter X"
-  x = gets.chomp
-  if not x.is_a? Fixnum then puts "Invalid input. X needs to be a real number."
+  x = gets.chomp.to_f
+  if x == 0 then puts "Invalid input. X needs to be a real number different than zero."
 end
-until p.is_a? Integer and p!=0 do
+end
+p=0
+# until p.is_a? Integer and p!=0 do
+# couldn't figure out how to take input from puts to number. 
+until p!=0 do
   puts "Enter P"
-  p = gets.chomp
-  if not p.is_a? Integer or p==0 then puts "Invalid input. P needs to be an integer number different than zero!"
+  p = gets.chomp.to_i
+  # if not p.is_a? Integer or p==0 then puts "Invalid input. P needs to be an integer number different than zero!"
+  if p==0 then puts "Invalid input. P needs to be an integer number different than zero!"
+end
 end
 
 sine(x,p)
